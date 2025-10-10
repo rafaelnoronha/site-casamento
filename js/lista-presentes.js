867b37ff327c553db710534c8d2df2b574dd6c53
rafaelnoronha <rafaelsnoronha@hotmail.com>
2025-10-09 23:11:15 -0300

    Muito tarde e muito sono, mas trabalhei

---
+++ js/lista-presentes.js
@@ -0,0 +1,42 @@
+const listapresentes = [
+    {
+        nome: 'Lava e Seca LG 1',
+        imagem: 'https://www.lg.com/content/dam/channel/wcms/br/images/lavadoras-de-roupas/cv5012wc4_abwfbrs_essp_br_c/450.jpg',
+        link: 'https://ge.globo.com/',
+    },
+    {
+        nome: 'Lava e Seca LG 2',
+        imagem: 'https://www.lg.com/content/dam/channel/wcms/br/images/lavadoras-de-roupas/cv5012wc4_abwfbrs_essp_br_c/450.jpg',
+        link: 'https://ge.globo.com/',
+    },
+    {
+        nome: 'Lava e Seca LG 3',
+        imagem: 'https://www.lg.com/content/dam/channel/wcms/br/images/lavadoras-de-roupas/cv5012wc4_abwfbrs_essp_br_c/450.jpg',
+        link: 'https://ge.globo.com/',
+    },
+    {
+        nome: 'Lava e Seca LG 4',
+        imagem: 'https://www.lg.com/content/dam/channel/wcms/br/images/lavadoras-de-roupas/cv5012wc4_abwfbrs_essp_br_c/450.jpg',
+        link: 'https://ge.globo.com/',
+    },
+    {
+        nome: 'Lava e Seca LG 5',
+        imagem: 'https://www.lg.com/content/dam/channel/wcms/br/images/lavadoras-de-roupas/cv5012wc4_abwfbrs_essp_br_c/450.jpg',
+        link: 'https://ge.globo.com/',
+    },
+    {
+        nome: 'Lava e Seca LG 6',
+        imagem: 'https://www.lg.com/content/dam/channel/wcms/br/images/lavadoras-de-roupas/cv5012wc4_abwfbrs_essp_br_c/450.jpg',
+        link: 'https://ge.globo.com/',
+    },
+    {
+        nome: 'Lava e Seca LG 7',
+        imagem: 'https://www.lg.com/content/dam/channel/wcms/br/images/lavadoras-de-roupas/cv5012wc4_abwfbrs_essp_br_c/450.jpg',
+        link: 'https://ge.globo.com/',
+    },
+    {
+        nome: 'Lava e Seca LG 8',
+        imagem: 'https://www.lg.com/content/dam/channel/wcms/br/images/lavadoras-de-roupas/cv5012wc4_abwfbrs_essp_br_c/450.jpg',
+        link: 'https://ge.globo.com/',
+    }
+]
